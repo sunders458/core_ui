6890de536a1518f26bbd0a9cfd66c78418afdc07
sunders458 <sunders458@gmail.com>
2023-12-14 15:49:50 +0000

    update formulaire login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -30,7 +30,7 @@
                 <input
                 class="form-input peer w-full rounded-lg border border-slate-300 bg-transparent px-3 py-2 pl-9 placeholder:text-slate-400/70 hover:z-10 hover:border-slate-400 focus:z-10 focus:border-primary dark:border-navy-450 dark:hover:border-navy-400 dark:focus:border-accent @error('phone') is-invalid @enderror"
                 placeholder="Enter Username"
-                type="email"
+                type="text"
                 value="{{ old('phone') }}" required autocomplete="email" autofocus
                 name="phone"
                 />
